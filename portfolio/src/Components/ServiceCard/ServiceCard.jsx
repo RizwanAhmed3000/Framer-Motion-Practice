c76ef7cc9eb2b0b56d78f45982471850309aab17
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-03-15 00:20:17 +0500

    service section in works

---
+++ portfolio/src/Components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,9 @@
+import './serviceCard.scss'
+
+const ServiceCard = () => {
+    return (
+        <div className='card'>ServiceCard</div>
+    )
+}
+
+export default ServiceCard
--- portfolio/src/Components/Services/Services.jsx
+++ portfolio/src/Components/Services/Services.jsx
@@ -1,8 +1,29 @@
+import ServiceCard from "../ServiceCard/ServiceCard"
 import "./services.scss"
+import { motion } from "framer-motion"
 
 const Services = () => {
     return (
-        <div className='services'>Services</div>
+        <div className='services'>
+            <div className="textContainer">
+                <p>
+                    I focus on helping your brand grow <br />
+                    and move forward
+                </p>
+                <hr />
+            </div>
+            <div className="titleContainer">
+                <div className="title">
+                    <h1><span>Unique</span> Ideas</h1>
+                </div>
+                <div className="title">
+                    <h1>For Your <span>Business</span></h1>
+                </div>
+            </div>
+            <div className="cardContainer">
+                <ServiceCard />
+            </div>
+        </div>
     )
 }
 
